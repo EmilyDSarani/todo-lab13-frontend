8177f2524a29eb5b9cf05b982eeaba0e2ed2c239
EmilyDSarani <emily.sarani@gmail.com>
2021-10-12 18:39:59 -0500

    need to make more commits, but sign up is officially done

--- src/signup/SignupRender.js
+++ src/signup/SignupRender.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { signUp } from '../fetch-utils';
 
 export default class SignupRender extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    state = {
+      password: '',
+      email: '',
+    };
+      handleSubmit = async e =>{
+        e.preventDefault();
+        const { token } = await signUp(this.state.email, this.state.password);
+        this.props.handleTokenChange(token);
+        this.props.history.push('/Todo');
+      };
+      
+      render() {
+        return (
+          <div>
+            <form onSubmit ={this.handleSubmit}>
+              <label>
+                Email Address:
+                <input value={this.state.email} onChange={(e)=> this.setState({ email:e.target.value })} type="email"/>
+              </label> 
+              <label>
+                Password:
+                <input value={this.state.password} onChange={(e)=> this.setState({ password:e.target.value })} type="password"/>
+              </label> 
+              <button>Submit</button>
+            </form>
+          </div>
+        );
+      }
 }
