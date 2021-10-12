71f774aecda337932253fe66195130e71c207eff
EmilyDSarani <emily.sarani@gmail.com>
2021-10-12 16:08:05 -0500

    i think setup is done

--- src/app/App.js
+++ src/app/App.js
@@ -1,50 +1,66 @@
 import { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+
 import Home from '../home/Home';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect
+  NavLink,
 } from 'react-router-dom';
 import './App.css';
+import Signup from '../signup/Signup';
+
+import Todo from '../todo/Todo';
+import Login from '../login/Login';
 
 class App extends Component {
 
   render() {
+    const TOKEN_KEY = TOKEN
+    state ={
+      token = localStorage.getItem(TOKEN_KEY) || ''
+    }
+    handleTokenChange = token => {
+      localStorage.setItem('TOKEN_KEY', token)
+      this.setState({token:token})
+    }
+    logout= () => {
+      localStorage.clear()
+      this.setState({token:''})
+    }
     return (
-      <div className="App">
-        <Router>
-          <Header/>
-          <main>
-
-            <Switch>
-              <Route path="/" exact={true}
-                render={routerProps => (
-                  <Home {...routerProps}/>
-                )}
-              />
-
-              <Route path="/resources" exact={true}
-                render={routerProps => (
-                  <div>Implement a page of resources</div>
-                )}
-              />
-
-              <Route path="/resources/:id"
-                render={routerProps => (
-                  <div>Implement a page for id {routerProps.match.params.id}</div>
-                )}
-              />
-
-              <Redirect to="/" />
-
-            </Switch>
-          </main>
-          <Footer/>
-        </Router>
-      </div>
+      <>
+      <Router>
+      <header className= 'not'>
+        <NavLink exact activeClassName='active'  to='/'>Home</NavLink> 
+        <NavLink exact activeClassName='active'  to='/SignUp'>SignUp</NavLink> 
+        <NavLink exact activeClassName='active' to='/Login'>SignIn</NavLink>
+        <NavLink exact activeClassName='active' to='/Todo'>SignIn</NavLink>
+      </header>
+      <Switch>
+        <Route
+        path="/"
+        exact
+        render={(routerProps) => <Home {...routerProps}/>}
+        />
+        <Route
+        path="/SignUp"
+        exact
+        render={(routerProps)=> <Signup {...routerProps}/>}
+        />
+        <Route
+        path="/Login"
+        exact
+        render={(routerProps)=> <Login {...routerProps}/>}
+        />
+         <Route
+        path="/Todo"
+        exact
+        render={(routerProps)=> <Todo {...routerProps}/>}
+        />
+      </Switch>
+      </Router>
+    </>
     );
   }
 
