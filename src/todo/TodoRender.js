49f1a8c5463fd22bdd4ce0512a266c58ece8f809
EmilyDSarani <emily.sarani@gmail.com>
2021-10-13 08:06:09 -0500

    code is there, but I am...missing something that will get this working...

--- src/todo/TodoRender.js
+++ src/todo/TodoRender.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import { getTodo } from '../fetch-utils';
+import { getTodo, postTodo } from '../fetch-utils';
 
 export default class TodoRender extends Component {
     state ={
       todo:[],
       todoChore: '',
     }
+    handleSubmit = async e =>{
+      e.preventDefault();
+      await postTodo(this.state.todoChore, this.props.token);
+      await getTodo(this.props.token);
+
+    }
     // I will need to call the getTodos, the putTodo and the postTodo.
     //Each will need a compondentDidMount?
     //Post will probably need a handle submit or something
     //putTodo will need to be able to toggle complete or not complete
     //getTodo will need to get the todo's associated to that account. 
+
     compondentDidMount = async () =>{
       const todo = await getTodo(this.props.token);
       this.setState({ todo });
@@ -18,7 +25,19 @@ export default class TodoRender extends Component {
     render() {
       return (
         <div>
-                
+          <form onSubmit = {this.handleSubmit}>
+            <label> 
+              <input value={this.state.todo} onChange={(e)=> this.setState({ todoChore: e.target.value })} />
+              <button>Add Todo</button>
+            </label>
+          </form>
+          
+          {/* not exactly sure how to render it here. maybe do an onClick...and...putTodo by id, completion, and token?
+              */}
+          {/* <div onClick= {asynce() => {await putTodo(todo.id, todo.completed, this.props.token)
+          }} >
+          
+        </div> */}
         </div>
       );
     }
