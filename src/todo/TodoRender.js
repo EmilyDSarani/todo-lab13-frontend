07e8c2bf88c321a129b5ccc987fd04bba9970480
EmilyDSarani <emily.sarani@gmail.com>
2021-10-13 07:44:23 -0500

    comment coding

--- src/todo/TodoRender.js
+++ src/todo/TodoRender.js
@@ -6,6 +6,11 @@ export default class TodoRender extends Component {
       todo:[],
       todoChore: '',
     }
+    // I will need to call the getTodos, the putTodo and the postTodo.
+    //Each will need a compondentDidMount?
+    //Post will probably need a handle submit or something
+    //putTodo will need to be able to toggle complete or not complete
+    //getTodo will need to get the todo's associated to that account. 
     compondentDidMount = async () =>{
       const todo = await getTodo(this.props.token);
       this.setState({ todo });
